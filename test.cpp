e2a1c6e36127502fa08a2c4f88948f2521950e56
HenryRuiHengWu <21910069@zju.edu.cn>
2020-11-03 16:44:52 +0800

    test1

---
+++ test.cpp
@@ -0,0 +1,27 @@
+//!MinGW/bin
+// -*- encoding: utf-8 -*-
+/*
+@File    :   test.cpp
+@Time    :   2020/11/03 12:33:34
+@Author  :   HenryWu 
+@Version :   1.0
+@Contact :   21910069@zju.edu.cn
+*/
+// Start typing your code from here
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=0;
+    int b=1;
+    cout<<"Hello Vscode1"<<endl;
+    cout<<"Hello Vscode2"<<endl;
+    cout<<"Hello Vscode3"<<endl;
+    a=5;
+    b=6;
+    cout<<"Hello Vscode4"<<endl;
+    cout<<"Hello Vscode5"<<endl;
+    system("pause");
+    return 0;
+}
